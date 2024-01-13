a6e95513b4ce8753171dcdbe3eb2d1fae0952b3b
Lenar Lima <lenar.toledo@gmail.com>
2024-01-12 21:53:48 -0400

    added connection with postgres using docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      // this load the entities in every change
+      autoLoadEntities: true,
+      //just use in mod-dev not production
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
