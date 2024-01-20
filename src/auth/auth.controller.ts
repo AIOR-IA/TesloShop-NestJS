d5310bf516b830a88256bf2faf641e5b1ba63ab8
Lenar Lima <lenar.toledo@gmail.com>
2024-01-19 23:03:45 -0400

    add custom guard to Roles in User

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req } from '@nestjs/common';
+import { Controller, Get, Post, Body, UseGuards, Req, Headers, SetMetadata } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto, LoginUserDto } from './dto';
 import { AuthGuard } from '@nestjs/passport';
-import { GetUser } from './decorators/get-user.decorator';
 import { User } from './entities/user.entity';
+import { GetUser, RawHeaders } from './decorators';
+import { IncomingHttpHeaders } from 'http';
+import { UserRoleGuard } from './guards/user-role/user-role.guard';
 
 
 @Controller('auth')
@@ -25,16 +27,28 @@ export class AuthController {
   @Get('private')
   @UseGuards( AuthGuard() )
   TestingPrivateRoute(
-    // @Req() request:Express.Request
-    @GetUser('email') user: User,
+    @Req() request:Express.Request,
+    @GetUser() user: User,
+    @GetUser('email') userEmail: string,
+    @RawHeaders() rawHeaders: string[],
+    @Headers() header: IncomingHttpHeaders
   ) {
-    console.log(user)
-
-
     return {
       ok: true,
       message: "valid token",
-      user
+      user,
+      userEmail,
+      rawHeaders,
+      header
     }
+  }
+
+  @Get('private2')
+  @SetMetadata('roles', ['admin','super-user'])
+  @UseGuards( AuthGuard(), UserRoleGuard )
+  privateRoute2(
+    @GetUser() user:User
+  ){
+    return user
   }
 }
