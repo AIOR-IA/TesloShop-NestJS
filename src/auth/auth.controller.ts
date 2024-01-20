d98b1f438c2e14fafbfc1b4a3b7a487b9505efaa
Lenar Lima <lenar.toledo@gmail.com>
2024-01-20 01:57:20 -0400

    added roleProtected with decorators

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,9 +3,11 @@ import { AuthService } from './auth.service';
 import { CreateUserDto, LoginUserDto } from './dto';
 import { AuthGuard } from '@nestjs/passport';
 import { User } from './entities/user.entity';
-import { GetUser, RawHeaders } from './decorators';
+import { Auth, GetUser, RawHeaders } from './decorators';
 import { IncomingHttpHeaders } from 'http';
 import { UserRoleGuard } from './guards/user-role/user-role.guard';
+import { RoleProtected } from './decorators/role-protected.decorator';
+import { ValidRoles } from './interfaces';
 
 
 @Controller('auth')
@@ -43,10 +45,21 @@ export class AuthController {
     }
   }
 
+  //using 2 decoratros to valid autorizacion role 
   @Get('private2')
-  @SetMetadata('roles', ['admin','super-user'])
+  // @SetMetadata('roles', ['admin','super-user']),  RoleProtected = asigna el metadata
+  @RoleProtected(ValidRoles.superUser, ValidRoles.user)
+  // AuthGuard() = verifica la autenticacion, UserRoleGuard=verifica que el usuario tenga el rol del metadata
   @UseGuards( AuthGuard(), UserRoleGuard )
   privateRoute2(
+    @GetUser() user:User
+  ){
+    return user
+  }
+
+  @Get('private3')
+  @Auth( ValidRoles.superUser)
+  privateRoute3(
     @GetUser() user:User
   ){
     return user
